84a65b7b02651fb552e3d292cf5ac58f31074e48
Krishna Kumar <149694348+KRISHNAKUMARPS2002@users.noreply.github.com>
2025-06-10 13:14:47 +0530

    Maths problems done

--- maths.js
+++ maths.js
@@ -21,7 +21,7 @@ function countDigitsOptimal(n) {
 countz = countDigitsOptimal(45748573987538538957398);
 console.log(countz);
 
-// Reverse Digits of A Number Brute
+// Reverse Digits of A Number
 function reverse(n) {
   revNum = 0;
 
@@ -54,3 +54,122 @@ function isPalindrome(n) {
 }
 
 console.log(isPalindrome(112));
+
+// Find GCD of two numbers
+// Brute Force Approach – Time: O(min(n1, n2))
+function findGcdBrute(n1, n2) {
+  let gcd = 1;
+  for (let i = 1; i <= Math.min(n1, n2); i++) {
+    if (n1 % i === 0 && n2 % i === 0) {
+      gcd = i;
+    }
+  }
+  return gcd;
+}
+
+console.log(findGcdBrute(20, 100));
+
+// Better Approach – Time: O(min(n1, n2))
+function findGcdBetter(n1, n2) {
+  for (let i = Math.min(n1, n2); i >= 1; i--) {
+    if (n1 % i === 0 && n2 % i === 0) {
+      return i;
+    }
+  }
+  return 1;
+}
+
+console.log(findGcdBetter(1000, 100));
+
+// Optimal Approach – Euclidean Algorithm – Time: O(log(min(n1, n2)))
+function findGcdOptimal(n1, n2) {
+  while (n1 > 0 && n2 > 0) {
+    if (n1 > n2) {
+      n1 = n1 % n2;
+    } else {
+      n2 = n2 % n1;
+    }
+  }
+  return n1 === 0 ? n2 : n1;
+}
+
+console.log(findGcdOptimal(1000, 100));
+
+// Armstrong Number (O(log₁₀N + 1))
+function isArmstrong(num) {
+  const power = num.toString().length;
+  let total = 0;
+  let n = num;
+
+  while (n > 0) {
+    const ld = n % 10;
+    total += Math.pow(ld, power);
+    n = Math.floor(n / 10);
+  }
+  return total === num;
+}
+
+console.log(isArmstrong(152));
+
+// Print All Divisors
+// Print All Divisors – Brute Force (O(N))
+function printDivisorsBrut(n) {
+  let divisors = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      divisors.push(i);
+    }
+  }
+  return divisors;
+}
+console.log(printDivisorsBrut(10));
+
+// Print All Divisors – Optimal (O(√N))
+function printDivisorsOptimal(n) {
+  let divisors = new Set();
+  let sqrt = Math.floor(Math.sqrt(n));
+
+  for (let i = 1; i <= sqrt; i++) {
+    if (n % i === 0) {
+      divisors.add(i);
+      divisors.add(n / i);
+    }
+  }
+
+  return Array.from(divisors).sort((a, b) => a - b);
+}
+
+console.log(printDivisorsOptimal(10));
+
+// Check Prime
+// Check Prime – Brute Force (O(N))
+function checkPrimeBrute(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      count += 1;
+    }
+  }
+  return count === 2;
+}
+console.log(checkPrimeBrute(10));
+
+// Check Prime – Optimal (O(√N))
+function checkPrimeOptimal(n) {
+  if (n < 2) return false;
+
+  let count = 0;
+  let sqrt = Math.floor(Math.sqrt(n));
+
+  for (let i = 0; i < sqrt; i++) {
+    if (n % i === 0) {
+      count++;
+      if (n / i != i) {
+        count++;
+      }
+    }
+  }
+  return count === 2;
+}
+
+console.log(checkPrimeOptimal(11));
